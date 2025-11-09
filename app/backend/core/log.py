05ea6eaa045160fb061882eb02d51e6e7bec3753
Taishi Furugo <cnj14125@ict.nitech.ac.jp>
2025-11-10 01:55:57 +0900

    plt.showを削除

--- app/backend/core/log.py
+++ app/backend/core/log.py
@@ -255,5 +255,5 @@ def plot_individual_params(population: list[dict], param_keys: list[str], genera
     plt.grid(True)
     plt.scatter(param_values1, param_values2)
     plt.savefig(file_path)
-    plt.show()
+    # plt.show()
     plt.close()
