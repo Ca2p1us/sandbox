d6f211e73c2e844c9e30efc9dead6ccc8641106f
Taishi Furugo <cnj14125@ict.nitech.ac.jp>
2025-12-17 03:15:43 +0900

    スライド用の画像をいろいろ集めた

--- backend/core/geneticAlgorithm/pre_selection.py
+++ backend/core/geneticAlgorithm/pre_selection.py
@@ -29,7 +29,7 @@ def select_top_individuals_by_pre_evaluation(population: List[dict], total_n: in
 
     selected_inds = sorted_population[:total_n]
 
-    # 上位と下位をそれぞれ選択
+    # # 上位と下位をそれぞれ選択
     # half_top = total_n // 2 + (total_n % 2)  # 奇数なら上位を1つ多く
     # half_bottom = total_n // 2
 
